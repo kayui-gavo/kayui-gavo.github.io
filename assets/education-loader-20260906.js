bd0cd2482321ccd4ccf426a8f057bf7490aeccd4
かゆい <128781460+kayui-gavo@users.noreply.github.com>
2026-09-19 03:28:09 +0900

    Stop forcing low-resolution course poster

--- assets/education-loader-20260906.js
+++ assets/education-loader-20260906.js
@@ -1,4 +1,3 @@
 (async()=>{
-  await import('/assets/education-base-20260906.js?v=20260919a');
-  await import('/assets/education-poster-source-hotfix-20260906.js?v=20260919a');
+  await import('/assets/education-base-20260906.js?v=20260919b');
 })();
